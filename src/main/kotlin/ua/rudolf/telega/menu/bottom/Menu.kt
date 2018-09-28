a57c9cb971da63ee21070cbf3fb75c69a3fd76da
rudolf <rudolfss13@gmail.com>
2018-09-28 23:07:29 +0300

    Move lib new version.

--- src/main/kotlin/ua/rudolf/telega/menu/bottom/Menu.kt
+++ src/main/kotlin/ua/rudolf/telega/menu/bottom/Menu.kt
@@ -4,9 +4,9 @@ import org.telegram.telegrambots.api.objects.Location
 import org.telegram.telegrambots.api.objects.Message
 import org.telegram.telegrambots.api.objects.Update
 import org.telegram.telegrambots.bots.AbsSender
-import ua.rudolf.telega.DefaultMessageSentCallback
-import ua.rudolf.telega.extractMessage
+import ua.rudolf.telega.menu.DefaultMessageSentCallback
 import ua.rudolf.telega.menu.TelegramCommand
+import ua.rudolf.telega.menu.extractMessage
 import ua.rudolf.telega.menu.keyboard
 
 fun <T> MenuPoint<T>.sendToUser(user: T, chatId: Long, f: (MenuPoint<T>) -> Unit, editMessage: Int? = null): List<TelegramCommand> {
--- src/main/kotlin/ua/rudolf/telega/menu/bottom/Session.kt
+++ src/main/kotlin/ua/rudolf/telega/menu/bottom/Session.kt
@@ -1,8 +1,8 @@
 package ua.rudolf.telega.menu.bottom
 
 import org.telegram.telegrambots.api.objects.Update
-import ua.rudolf.telega.extractMessage
 import ua.rudolf.telega.menu.TelegramCommand
+import ua.rudolf.telega.menu.extractMessage
 import ua.rudolf.telega.menu.textMessage
 
 class Session<T>(val userSession: T, val menu: Menu<T>) {
