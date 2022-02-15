e481cc0fb20531ec0e5480ee3d51700983eb8e41
rudolf <rudolfss13@gmail.com>
2022-02-15 15:17:52 +0300

    build fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,18 @@ dependencies {
 
 publishing {
     repositories {
-        maven(url = "build/repository")
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
     }
 }
--- sample/src/main/kotlin/ua/rudolf/sample/Main.kt
+++ sample/src/main/kotlin/ua/rudolf/sample/Main.kt
@@ -2,6 +2,8 @@ package ua.rudolf.sample
 
 import org.telegram.telegrambots.ApiContextInitializer
 import org.telegram.telegrambots.TelegramBotsApi
+import ua.rudolf.telega.menu.TelegramBotConfig
+import ua.rudolf.telega.menu.TelegramBotProxyConfig
 
 val config = TelegramBotConfig(
         telegramBotToken = "BOT_TOKEN",
